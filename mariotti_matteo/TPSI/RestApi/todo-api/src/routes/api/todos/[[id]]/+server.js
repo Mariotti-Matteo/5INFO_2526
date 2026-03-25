5fa6b960d8861f9bd9deb69300fd2a23a4f0e3be
Mariotti-Matteo <matteo.mariotti@istitutoagnelli.it>
2026-03-25 08:57:02 +0100

    Esercitazione 2,3,4

--- mariotti_matteo/TPSI/RestApi/todo-api/src/routes/api/todos/[[id]]/+server.js
+++ mariotti_matteo/TPSI/RestApi/todo-api/src/routes/api/todos/[[id]]/+server.js
@@ -49,10 +49,11 @@ export async function GET({params, request, url}) {
     const sql_azione4 = db.prepare("SELECT * FROM todo WHERE done = ?");
     const sql_azione5 = db.prepare("SELECT * FROM todo WHERE priority = ?");
 
-    const exec_query = (azione, param) => {
-
-        const todo = param || parma == 0 ? azione.all(param) : azione.all()
-        if(todo.lenght > 0)
+    const exec_query = (azione, params) => {
+        console.log("4")
+        const todo = params || params == 0 ? azione.all(params) : azione.all()
+        console.log("5")
+        if(todo.length > 0)
             return json(todo, {status:200})
         else
             return json({}, {status: 404})
@@ -62,16 +63,20 @@ export async function GET({params, request, url}) {
         if(params.id){
             return exec_query(sql_azione3, params.id)
         }
+        console.log("1")
 
         if(url.searchParams.has("priority")) {
             return exec_query(sql_azione5, +url.searchParams.get("priority"));
         }
 
+        console.log("2")
+
         if(url.searchParams.has("done")) {
             return exec_query(sql_azione4, +JSON.parse(url.searchParams.get("done")));
         }
 
-        return exec_query(sql_azione2);
+        console.log("3")
+        return exec_query(sql_azione2, params);
 
     } catch(e) {
         return json({}, { status:500});
@@ -79,6 +84,31 @@ export async function GET({params, request, url}) {
 
 }
 
+// export async function POST({request}) {
+//     try {
+//         const body = await request.json();
+//         console.log("Ricevuto HTTP POST con body", body)
+
+//         const sql_azione = db.prepare("INSERT INTO todo (task, done, priority) VALUES(@task, @done, @priority)");
+
+//         const res = sql_azione.run({
+//             task: body.task,
+//             done: +body.done,
+//             priority: +body.priority,
+//         });
+
+//         if (res.changes == 1) {
+//             body["id"] = res.lastInsertRowid;
+//             return json(body, {
+//                 status: 201,
+//                 headers: new Headers({"Location": `http://localhost:5173/api/todos/${body["id"]}`})
+//             })
+//         }
+//     } catch (e) {
+//     console.log(e)
+//     return json({}, {status:500})
+// }
+// }
 
 // export async function GET({params, request, url}) {
     
